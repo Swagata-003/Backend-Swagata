60e83c1819a442b0dbda30398e231f637c7b18fb
Swagata Roy Chowdhury <swagataroychowdhury11@gmail.com>
2024-10-21 03:13:05 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`Server is running at port:${process.env.PORT}`);
+  })
+})
+.catch((err)=>{
+  console.lob("MONGODB CONNECTION FAILED!!",err);
+})
+
+
 
 
 /*
